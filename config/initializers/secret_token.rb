0ee8f4b46d3d16c569cf3855c2a890f0bedeb7ad
chsieh6 <charles.hsieh6@gmail.com>
2013-07-07 09:05:09 -0700

    homepage create

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Triviadojo::Application.config.secret_key_base = 'b1716428e265e541990b0dd6035a9b23c11488e0c7d5017fba78017c390b46b3bdd8cb24a9bee9565a91ce09fa919a132fb8d5b694fa3f3f04e6005130f411d2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Triviadojo::Application.config.session_store :cookie_store, key: '_triviadojo_session'
